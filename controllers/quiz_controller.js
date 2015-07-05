bc78b3561ad06715c2f31ee2e42c51486e4431f5
fra03r <fcojavierra@gmail.com>
2015-07-05 20:07:27 +0200

    Primera pregunta

---
+++ controllers/quiz_controller.js
@@ -0,0 +1,16 @@
+
+exports.question = function(req, res){
+    res.render('quizes/question', {pregunta: 'Capital de Italia'});
+};
+
+exports.answer = function(req, res){
+    if (req.query.respuesta === 'Roma') {
+      res.render('quizes/answer', {respuesta: 'Correcto'});
+    } else {
+      res.render('quizes/answer', {respuesta: 'Incorrecto'});
+    };
+};
+
+
+
+//  console.log("req.quiz.id: " + req.quiz.id);
