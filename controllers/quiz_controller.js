2c530ae890ffc440af023cca678a994cd5aa5133
fra03r <fcojavierra@gmail.com>
2015-07-09 18:47:08 +0200

    Añado funcionalidad de crear pregunta

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -2,6 +2,7 @@ var models = require('../models/models.js');
 
 // Autoload - factoriza el código si ruta incluye :quizId
 exports.load = function(req, res, next, quizId) {
+  console.log("Entra en load= " + quizId);
   models.Quiz.find(quizId).then(
     function(quiz) {
       if (quiz) {
@@ -33,7 +34,8 @@ exports.index = function(req, res) {
 
 // GET /quizes/:id
 exports.show = function(req, res){
-    res.render('quizes/show', {quiz: req.quiz})
+    console.log("Entra en show= " + req.quiz);
+    res.render('quizes/show', {quiz: req.quiz});
 };
 
 // GET /quizes/:id/answer
@@ -43,4 +45,22 @@ exports.answer = function(req, res){
       resultado = 'Correcto';
   }
   res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
+};
+
+// GET /quizes/new
+exports.new = function(req, res) {
+  var quiz = models.Quiz.build( // crea objeto quiz
+    {pregunta: "Pregunta", respuesta: "Respuesta"}
+  );
+  res.render('quizes/new', {quiz: quiz});
+};
+
+
+// POST /quizes/create
+exports.create = function(req, res) {
+  var quiz = models.Quiz.build( req.body.quiz );
+  //guarda en DB los campos pregunta y respuesta de quiz
+  quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+    res.redirect('/quizes');
+  }) // Redirección HTTP (URL relativo)
 };
