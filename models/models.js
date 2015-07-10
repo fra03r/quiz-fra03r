ac2d03cdcad0e7b5c32c61ad619b4bb8e46a3e54
fra03r <fcojavierra@gmail.com>
2015-07-10 18:50:12 +0200

    Se añade funcionalidad de añadir comentario

--- models/models.js
+++ models/models.js
@@ -26,9 +26,20 @@ var sequelize = new Sequelize(DB_name, user, pwd,
   }
 );
 
-// Importar definicion de la tabla Quiz en quiz.js
-var Quiz = sequelize.import(path.join(__dirname,'quiz'));
-exports.Quiz = Quiz;//exportar definición de tabla Quiz
+// Importar definicion de la tabla Quiz
+var quiz_path = path.join(__dirname,'quiz');
+var Quiz = sequelize.import(quiz_path);
+
+// Importar definicion de la tabla Comment
+var comment_path = path.join(__dirname,'comment');
+var Comment = sequelize.import(comment_path);
+
+Comment.belongsTo(Quiz);
+Quiz.hasMany(Comment);
+
+// exportar tablas
+exports.Quiz = Quiz;
+exports.Comment = Comment;
 
 // sequelize.sync() inicializa tabla de preguntas en DB
 sequelize.sync().then(function() {
